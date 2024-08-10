cdf4d9012ea240769f3fcb58cdd7cda14064d8d2
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-10 19:46:09 +0200

    add theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: '#F1F6FC',    // pearl white (now used as background)
+          secondary: '#313B37',  // street gray (now used for text)
+          accent: '#B6FA46',     // lime (still used for accents)
+          dark: '#011109',       // almost black (now used for contrast elements)
+        },
+      },
     },
     plugins: [],
   }
