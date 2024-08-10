0aef1b76ed12889fbb591e89c7890600f9f8d1aa
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-11 00:17:48 +0200

    update progress bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://upnotify.me',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), sitemap(), alpinejs()],
   content: {
-    collections: [
-      {
-        name: 'blog',
-        type: 'content',
-      },
-    ],
-  },
+    collections: [{
+      name: 'blog',
+      type: 'content'
+    }]
+  }
 });
