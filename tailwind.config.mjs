0f2f99af4570359158ba3adec5ebe1744b7f52e4
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-11 09:18:31 +0200

    change CTA

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ module.exports = {
         colors: {
           primary: '#F1F6FC',    // pearl white (now used as background)
           secondary: '#313B37',  // street gray (now used for text)
-          accent: '#B6FA46',     // lime (still used for accents)
+          accent: '#00FF66',     // lime (still used for accents)
           dark: '#011109',       // almost black (now used for contrast elements)
         },
       },
