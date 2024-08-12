8a832504b1243e3326c1e69a473c73863079b744
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-12 20:03:26 +0200

    redesign navbar and hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,15 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-          primary: '#F1F6FC',    // pearl white (now used as background)
-          secondary: '#313B37',  // street gray (now used for text)
-          accent: '#00FF66',     // lime (still used for accents)
-          dark: '#011109',       // almost black (now used for contrast elements)
+          pearl: "#F1F6FC",
+          black: "#011109",
+          dark: "#313B37",
+          lime: "#00FF66",
+          gold: "#FFD700"
+          // primary: '#F1F6FC',    // pearl white (now used as background)
+          // secondary: '#313B37',  // street gray (now used for text)
+          // accent: '#00FF66',     // lime (still used for accents)
+          // dark: '#011109',       // almost black (now used for contrast elements)
         },
       },
     },
