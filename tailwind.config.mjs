a8067a9b0e58c51a47aa379ecf19d462341df377
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-12 21:03:59 +0200

    add how to section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
       extend: {
+        boxShadow: {
+          'gold': '4px 4px 0px 0px rgba(255, 215, 0, 1)',
+          'gold-lg': '8px 8px 0px 0px rgba(255, 215, 0, 1)',
+          'dark': '4px 4px 0px 0px rgba(49,59,55, 1)',
+          'dark-lg': '8px 8px 0px 0px rgba(49,59,55, 1)',
+          'lime-lg': '8px 8px 0px 0px rgba(0,255,102, 1)',
+        },
         colors: {
           pearl: "#F1F6FC",
           black: "#011109",
