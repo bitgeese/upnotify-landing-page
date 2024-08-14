7e0ebe3a2d8fc10b6a2ffa52f71f2a5c487f7519
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-14 15:49:21 +0200

    add pricing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,7 @@ module.exports = {
           'gold-lg': '8px 8px 0px 0px rgba(255, 215, 0, 1)',
           'dark': '4px 4px 0px 0px rgba(49,59,55, 1)',
           'dark-lg': '8px 8px 0px 0px rgba(49,59,55, 1)',
+          'lime': '2px 2px 0px 0px rgba(0,255,102, 1)',
           'lime-lg': '8px 8px 0px 0px rgba(0,255,102, 1)',
         },
         colors: {
