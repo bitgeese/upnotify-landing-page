330547a4db3fb44a036db76c03cfbb348984af61
Maciej Janowski <maciej.janowski1@icloud.com>
2024-08-14 16:19:11 +0200

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://upnotify.me',
-  integrations: [tailwind(), sitemap(), alpinejs()],
+  integrations: [tailwind(), sitemap(), alpinejs(), mdx()],
   content: {
     collections: [{
       name: 'blog',
